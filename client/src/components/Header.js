90178be1bda4d86b3a60c29e40dde1439f19cfcc
camcfarlin <camcfarlin@gmail.com>
2018-10-27 12:46:08 -0600

    bootstrap dependency

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
+import {Navbar, Nav, NavItem,NavDropdown,MenuItem} from 'react-bootstrap';
 
 class Header extends Component {
   renderContent () {
@@ -20,36 +21,40 @@ class Header extends Component {
     render () {
     // console.log(this.props);
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark navbar-fixed-top">      
-      <Link to={this.props.auth ? '/portfolio': '/'}
-        className="navbar-brand" href="/"
-        >
-        CyberTrader
-        </Link>
-        <button  className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <a className="nav-item nav-link active" href="#">
-              Home <span className="sr-only">(current)</span>
-            </a>
-            <a className="nav-item nav-link" href="#">About Us</a>
-
-            {this.renderContent ()}
-
-            {/* href temporary ^ */}
-            {/* add to className - disabled */}
-          </div>
-        </div>
-      </nav>
+      <Navbar inverse collapseOnSelect>
+      <Navbar.Header>
+        <Navbar.Brand>
+        <Link to={this.props.auth ? '/portfolio': '/'}
+            className="navbar-brand" href="/"
+            >
+            CyberTrader
+            </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav>
+          <NavItem eventKey={1} href="#">
+            Link
+          </NavItem>
+          <NavItem eventKey={2} href="#">
+            Link
+          </NavItem>
+          <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+            <MenuItem eventKey={3.1}>Action</MenuItem>
+            <MenuItem eventKey={3.2}>Another action</MenuItem>
+            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey={3.3}>Separated link</MenuItem>
+          </NavDropdown>
+        </Nav>
+        <Nav pullRight>
+          <NavItem eventKey={1} href="#">
+          {this.renderContent ()}
+          </NavItem>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
     );
   }
 }
@@ -57,3 +62,37 @@ function mapStateToProps({auth}) {
   return {auth};
 }
 export default connect (mapStateToProps) (Header);
+
+
+
+
+{/* <nav className="navbar navbar-expand-lg navbar-dark bg-dark navbar-fixed-top">      
+<Link to={this.props.auth ? '/portfolio': '/'}
+  className="navbar-brand" href="/"
+  >
+  CyberTrader
+  </Link>
+  <button  className="navbar-toggler"
+    type="button"
+    data-toggle="collapse"
+    data-target="#navbarNavAltMarkup"
+    aria-controls="navbarNavAltMarkup"
+    aria-expanded="false"
+    aria-label="Toggle navigation"
+  >
+    <span className="navbar-toggler-icon" />
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div className="navbar-nav">
+      <a className="nav-item nav-link active" href="#">
+        Home <span className="sr-only">(current)</span>
+      </a>
+      <a className="nav-item nav-link" href="#">About Us</a>
+
+      
+
+      {/* href temporary ^ */}
+      {/* add to className - disabled */}
+//     </div>
+//   </div>
+// </nav> */}
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import {Jumbotron, Button} from 'react-bootstrap';
 
-const Landing = () => {
+const Landing = props => {
   return (
-    <div style={{ textAlign:'center'}}>
-      <h1>
-        CyberTrader
-      </h1>
-      Paper trading for newbs
+    <div>
+      <Jumbotron>
+        <h1 className="display-3">CyberTrader</h1>
+        <p className="lead">
+          Trading in a safe enviornment, Learn and compete with friends! Paper trading for newbs</p>
+        <p className="lead">
+          <Button color="primary">Learn More</Button>
+        </p>
+      </Jumbotron>
     </div>
   );
 };
